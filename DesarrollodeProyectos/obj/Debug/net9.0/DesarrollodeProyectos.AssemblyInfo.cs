cf1d9628266b58ca2f92881108c3a38498eec3b4
Angel Ibarra <al44361@un.edu.mx>
2025-03-26 02:40:58 -0600

    Actualizacion 26/03/2025

--- DesarrollodeProyectos/obj/Debug/net9.0/DesarrollodeProyectos.AssemblyInfo.cs
+++ DesarrollodeProyectos/obj/Debug/net9.0/DesarrollodeProyectos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesarrollodeProyectos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538764ae88142bf46774e1c4c5f38ab75295707d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3186d1880d1767867024c4c7ad432d4c5d11b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesarrollodeProyectos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesarrollodeProyectos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
